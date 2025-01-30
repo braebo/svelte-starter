06f28826c67a676cb9586881341fbb601d051acf
Braden Wiggins <braden@fractal-hq.com>
2025-01-29 23:12:49 -0500

    feat: view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import { setupViewTransition } from 'sveltekit-view-transition'
 	import Shell from '$lib/components/Shell.svelte'
 	import Nav from '$lib/components/nav/Nav.svelte'
 	import Gooey from '$lib/components/Gooey.svelte'
 	import { BROWSER, DEV } from 'esm-env'
 	import '../styles/app.scss'
+
+	setupViewTransition()
 
 	let { data, children: layout_children } = $props()
 </script>
