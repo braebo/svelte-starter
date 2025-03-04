140a1d0fc524f5ff3316337fdbed990a0db61596
Braden Wiggins <braden@fractal-hq.com>
2025-03-03 21:07:58 -0500

    smh: hooks

---
+++ src/hooks.client.ts
@@ -0,0 +1,34 @@
+import type { Handle, HandleClientError } from '@sveltejs/kit'
+
+import { redirect } from '@sveltejs/kit'
+import { router } from '$lib/router'
+
+import { parse } from 'cookie'
+import { DEV } from 'esm-env'
+
+export const handle: Handle = ({ event, resolve }) => {
+	const cookies = parse(event.request.headers?.get('cookie') || '')
+	event.locals.theme = <'dark' | 'light' | 'system'>cookies.theme || 'dark'
+
+	const route = router.get(event.url.pathname)
+	if (!DEV && (route as any)?.dev) {
+		redirect(308, '/404')
+	}
+
+	let page = ''
+	return resolve(event, {
+		transformPageChunk: ({ html, done }) => {
+			page += html
+			if (done)
+				return page.replace('__THEME__', event.locals.theme).replace('__COLOR_SCHEME__', event.locals.theme)
+		},
+	})
+}
+
+export const handleError: HandleClientError = ({ error, event }) => {
+	console.error(error)
+
+	if (error && typeof error === 'object' && 'status' in error && error.status === 404) {
+		if (event.url.pathname !== '/404') redirect(308, '/404')
+	}
+}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,6 @@
-import { error, type Handle } from '@sveltejs/kit'
+import type { Handle, HandleServerError } from '@sveltejs/kit'
+
+import { redirect } from '@sveltejs/kit'
 import { router } from '$lib/router'
 
 import { parse } from 'cookie'
@@ -8,14 +10,9 @@ export const handle: Handle = ({ event, resolve }) => {
 	const cookies = parse(event.request.headers?.get('cookie') || '')
 	event.locals.theme = <'dark' | 'light' | 'system'>cookies.theme || 'dark'
 
-	try {
-		const route = router.get(event.url.pathname)
-		if (route && (route as any)?.dev && !DEV) {
-			error(404, 'Not found')
-		}
-	} catch (e) {
-		console.error(e)
-		error(404, 'Not found')
+	const route = router.get(event.url.pathname)
+	if (!DEV && (route as any)?.dev) {
+		redirect(308, '/404')
 	}
 
 	let page = ''
@@ -26,4 +23,12 @@ export const handle: Handle = ({ event, resolve }) => {
 				return page.replace('__THEME__', event.locals.theme).replace('__COLOR_SCHEME__', event.locals.theme)
 		},
 	})
+}
+
+export const handleError: HandleServerError = ({ error, event }) => {
+	console.error(error)
+
+	if (error && typeof error === 'object' && 'status' in error && error.status === 404) {
+		if (event.url.pathname !== '/404') redirect(308, '/404')
+	}
 }
